e8fa613cffe8535ee0c9c43229f09d240a920e07
Milos Zarkovic <milos.zarkovic@kupujemprodajem.com>
2017-10-10 09:42:21 +0200

    init commit

---
+++ index.js
@@ -0,0 +1,10 @@
+var http = require("http");
+
+var server = http.createServer(function(request, response) {
+  response.writeHead(200, {"Content-Type": "text/html"});
+  response.write("Hello World!");
+  response.end();
+});
+
+server.listen(3000);
+console.log("Server is listening on port 3000");
